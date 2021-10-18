4d18f7d1aae9043fbbbee41e68f73877254905a1
tandelpraniket <tandelpraniket@gmail.com>
2021-10-18 11:57:05 +0530

    Git Jen Modified

--- mycode.java
+++ mycode.java
@@ -1,6 +1,6 @@
 class mycode{
 public static void main(String args[])
 { 
-               System.out.println("Git Jenkind Integration");
+               System.out.println("Git Jenkis Integration");
 }         
 }
